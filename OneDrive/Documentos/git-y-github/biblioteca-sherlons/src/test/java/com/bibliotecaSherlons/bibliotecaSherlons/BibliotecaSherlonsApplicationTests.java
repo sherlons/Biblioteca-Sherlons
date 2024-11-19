d9dc6bfcc0db0bf6982c099a54c8492f20169372
sherlons <lukasteoo03@gmail.com>
2024-11-18 20:28:02 -0500

    creacion main

---
+++ OneDrive/Documentos/git-y-github/biblioteca-sherlons/src/test/java/com/bibliotecaSherlons/bibliotecaSherlons/BibliotecaSherlonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotecaSherlons.bibliotecaSherlons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaSherlonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
